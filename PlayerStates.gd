0ba65c1ca4889f8ac75d330dca4f6c746ddad168
PEDRO VICTOR <48770714+PedrovictorSouza@users.noreply.github.com>
2024-04-30 07:10:31 -0300

    player states started

---
+++ PlayerStates.gd
@@ -0,0 +1,18 @@
+extends Node
+class_name PlayerStates
+
+
+func enter(_player):
+	print("ai papai")
+
+func exit(player):
+	pass
+
+func handle_input(player, event):
+	pass
+
+func update(player, delta):
+	pass
+	
+func _ready():
+	enter(self)
